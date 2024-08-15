7efcc65699657c80b1c0482e3a37b9fd158bbad4
Pascal MIETLICKI <p.mietlicki@gmail.com>
2024-08-15 21:09:51 +1100

    memcached management

--- memcached.sh
+++ memcached.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-exec gosu memcache /usr/bin/memcached -m ${memcached:-64}
+
+# Vérifie si la variable memcached est définie à "false"
+if [ "$memcached" = "false" ]; then
+  echo "Memcached is disabled."
+else
+  # Définit la valeur par défaut à 64 MiB si aucune valeur n'est spécifiée
+  exec gosu memcache /usr/bin/memcached -m ${memcached:-64}
+fi
