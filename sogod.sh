792c34dd1a6e4ad5f668767924c902e8c9a09b47
Pascal MIETLICKI <p.mietlicki@gmail.com>
2024-08-16 20:02:48 +1100

    Update sogod.sh

--- sogod.sh
+++ sogod.sh
@@ -17,16 +17,80 @@ if [ ! -f "$CONFIG_PATH" ]; then
     cp "$DEFAULT_CONFIG" "$CONFIG_PATH"
 
     # Apply environment variables if they are defined
-    awk -v imap="$SOGoIMAPServer" -v smtp="$SOGoSMTPServer" -v mysql_host="$MYSQL_HOST" -v mysql_password="$MYSQL_PASSWORD" -v pg_host="$POSTGRESQL_HOST" -v pg_password="$POSTGRESQL_PASSWORD" '
-    {
-        if ($1 == "SOGoIMAPServer") $3 = "\""imap"\";";
-        if ($1 == "SOGoSMTPServer") $3 = "\""smtp"\";";
-        gsub(/host.docker.internal/, mysql_host);
-        gsub(/sogoPassword/, mysql_password);
-        gsub(/host.docker.internal/, pg_host);
-        gsub(/sogoPassword/, pg_password);
-        print;
-    }' "$CONFIG_PATH" > "$CONFIG_PATH.tmp" && mv "$CONFIG_PATH.tmp" "$CONFIG_PATH"
+    awk -v imap="${MAIL_IMAP_SERVER:-127.0.0.1}" \
+        -v smtp="${MAIL_SMTP_SERVER:-127.0.0.1}" \
+        -v sieve="${MAIL_SIEVE_SERVER:-sieve://127.0.0.1:4190}" \
+        -v mail_domain="${MAIL_DOMAIN:-example.com}" \
+        -v drafts_folder="${MAIL_DRAFTS_FOLDER_NAME:-Drafts}" \
+        -v sent_folder="${MAIL_SENT_FOLDER_NAME:-Sent}" \
+        -v trash_folder="${MAIL_TRASH_FOLDER_NAME:-Trash}" \
+        -v sieve_enabled="${MAIL_SIEVE_SCRIPTS_ENABLED:-YES}" \
+        -v vacation_enabled="${MAIL_VACATION_ENABLED:-YES}" \
+        -v forward_enabled="${MAIL_FORWARD_ENABLED:-YES}" \
+        -v sieve_encoding="${MAIL_SIEVE_FOLDER_ENCODING:-UTF-8}" \
+        -v password_change="${MAIL_PASSWORD_CHANGE_ENABLED:-NO}" \
+        -v spool_path="${MAIL_SPOOL_PATH:-/var/spool/sogo}" \
+        -v mail_check="${MAIL_MESSAGE_CHECK:-manually}" \
+        -v appointment_notifications="${MAIL_APPOINTMENT_SEND_EMAIL_NOTIFICATIONS:-YES}" \
+        -v enable_alarms="${MAIL_ENABLE_EMAIL_ALARMS:-YES}" \
+        -v sogo_language="${SOGO_LANGUAGE:-French}" \
+        -v sogo_timezone="${SOGO_TIMEZONE:-Europe/Paris}" \
+        -v sogo_page_title="${SOGO_PAGE_TITLE:-My SOGo Server}" \
+        -v sogo_login_title="${SOGO_LOGIN_TITLE:-Welcome to SOGo}" \
+        -v debug_requests="${SOGO_DEBUG_REQUESTS:-YES}" \
+        -v debug_base_url="${SOGO_DEBUG_BASE_URL:-YES}" \
+        -v refresh_view_check="${SOGO_REFRESH_VIEW_CHECK:-every_5_minutes}" \
+        -v refresh_on_foreground="${SOGO_REFRESH_VIEW_ON_FOREGROUND:-YES}" \
+        -v reply_placement="${SOGO_MAIL_REPLY_PLACEMENT:-below}" \
+        -v signature_placement="${SOGO_MAIL_SIGNATURE_PLACEMENT:-below}" \
+        -v forwarding_method="${SOGO_MAIL_MESSAGE_FORWARDING:-inline}" \
+        -v logging_level="${SOGO_LOGGING_LEVEL:-debug}" \
+        -v mysql_host="${MYSQL_HOST:-host.docker.internal}" \
+        -v mysql_user="${MYSQL_USER:-sogo}" \
+        -v mysql_password="${MYSQL_PASSWORD:-sogoPassword}" \
+        -v mysql_db="${MYSQL_DATABASE:-sogo}" \
+        -v pg_host="${POSTGRESQL_HOST:-host.docker.internal}" \
+        -v pg_user="${POSTGRESQL_USER:-sogo}" \
+        -v pg_password="${POSTGRESQL_PASSWORD:-sogoPassword}" \
+        -v pg_db="${POSTGRESQL_DATABASE:-sogo}" \
+        '{
+            gsub(/SOGoIMAPServer = "127.0.0.1";/, "SOGoIMAPServer = \"" imap "\";");
+            gsub(/SOGoSMTPServer = "127.0.0.1";/, "SOGoSMTPServer = \"" smtp "\";");
+            gsub(/SOGoSieveServer = "sieve:\/\/127.0.0.1:4190";/, "SOGoSieveServer = \"" sieve "\";");
+            gsub(/SOGoMailDomain = "example.com";/, "SOGoMailDomain = \"" mail_domain "\";");
+            gsub(/SOGoDraftsFolderName = Drafts;/, "SOGoDraftsFolderName = \"" drafts_folder "\";");
+            gsub(/SOGoSentFolderName = Sent;/, "SOGoSentFolderName = \"" sent_folder "\";");
+            gsub(/SOGoTrashFolderName = Trash;/, "SOGoTrashFolderName = \"" trash_folder "\";");
+            gsub(/SOGoSieveScriptsEnabled = YES;/, "SOGoSieveScriptsEnabled = " sieve_enabled ";");
+            gsub(/SOGoVacationEnabled = YES;/, "SOGoVacationEnabled = " vacation_enabled ";");
+            gsub(/SOGoForwardEnabled = YES;/, "SOGoForwardEnabled = " forward_enabled ";");
+            gsub(/SOGoSieveFolderEncoding = UTF-8;/, "SOGoSieveFolderEncoding = \"" sieve_encoding "\";");
+            gsub(/SOGoPasswordChangeEnabled = NO;/, "SOGoPasswordChangeEnabled = " password_change ";");
+            gsub(/SOGoMailSpoolPath = "\/var\/spool\/sogo";/, "SOGoMailSpoolPath = \"" spool_path "\";");
+            gsub(/SOGoMailMessageCheck = manually;/, "SOGoMailMessageCheck = " mail_check ";");
+            gsub(/SOGoAppointmentSendEMailNotifications = YES;/, "SOGoAppointmentSendEMailNotifications = " appointment_notifications ";");
+            gsub(/SOGoEnableEMailAlarms = YES;/, "SOGoEnableEMailAlarms = " enable_alarms ";");
+            gsub(/SOGoLanguage = French;/, "SOGoLanguage = \"" sogo_language "\";");
+            gsub(/SOGoTimeZone = Europe\/Paris;/, "SOGoTimeZone = \"" sogo_timezone "\";");
+            gsub(/SOGoPageTitle = "My SOGo Server";/, "SOGoPageTitle = \"" sogo_page_title "\";");
+            gsub(/SOGoLoginTitle = "Welcome to SOGo";/, "SOGoLoginTitle = \"" sogo_login_title "\";");
+            gsub(/SOGoDebugRequests = YES;/, "SOGoDebugRequests = \"" debug_requests "\";");
+            gsub(/SOGoDebugBaseURL = YES;/, "SOGoDebugBaseURL = \"" debug_base_url "\";");
+            gsub(/SOGoRefreshViewCheck = every_5_minutes;/, "SOGoRefreshViewCheck = " refresh_view_check ";");
+            gsub(/SOGoRefreshViewOnForeground = YES;/, "SOGoRefreshViewOnForeground = " refresh_on_foreground ";");
+            gsub(/SOGoMailReplyPlacement = below;/, "SOGoMailReplyPlacement = " reply_placement ";");
+            gsub(/SOGoMailSignaturePlacement = below;/, "SOGoMailSignaturePlacement = " signature_placement ";");
+            gsub(/SOGoMailMessageForwarding = inline;/, "SOGoMailMessageForwarding = " forwarding_method ";");
+            gsub(/SOGoLoggingLevel = debug;/, "SOGoLoggingLevel = \"" logging_level "\";");
+
+            # Replace MySQL URLs
+            gsub(/mysql:\/\/sogo:sogoPassword@host.docker.internal:3306\/sogo/, "mysql://" mysql_user ":" mysql_password "@" mysql_host ":3306/" mysql_db);
+
+            # Replace PostgreSQL URLs
+            gsub(/postgresql:\/\/sogo:sogoPassword@host.docker.internal:5432\/sogo/, "postgresql://" pg_user ":" pg_password "@" pg_host ":5432/" pg_db);
+
+            print;
+        }' "$CONFIG_PATH" > "$CONFIG_PATH.tmp" && mv "$CONFIG_PATH.tmp" "$CONFIG_PATH"
 
     # Save the original for reference
     cp "$CONFIG_PATH" /srv/etc/sogo.conf.orig
